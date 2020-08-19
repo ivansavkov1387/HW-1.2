3fb27498259ca559ac1927a0af7b033c3df76ee0
Ivan Savkov <ivansavkov1387@gmail.com>
2020-08-19 19:32:25 +0300

    Initial Commit

---
+++ Svetofor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Svetofor
+//
+//  Created by Иван on 8/19/20.
+//  Copyright © 2020 Ivan Savkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
